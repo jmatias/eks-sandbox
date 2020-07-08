168d8bc2ab289993d45934d62f55956c9785c48d
Javier Matias <jmatias@thoughtworks.com>
2020-07-07 22:17:46 -0400

    Updated bootstrap

--- hack/bootstrap.sh
+++ hack/bootstrap.sh
@@ -2,6 +2,8 @@
 
 SCRIPT_ROOT=$(dirname "${BASH_SOURCE}")/..
 
+$SCRIPT_ROOT/hack/configure.sh
+
 if [[ -z "${GITHUB_TOKEN}" ]]; then
   echo "Environment variable GITHUB_TOKEN must be set."
   exit 1
@@ -21,6 +23,7 @@ function restartDeployment() {
 }
 
 function waitForDeployment() {
+  set +e
   namespace=$1
   deploymentName=$2
 
@@ -30,54 +33,75 @@ function waitForDeployment() {
   done
 }
 
-eksctl get cluster $(getClusterName) >/dev/null 2>&1
-retVal=$?
-set -e
-if [ $retVal -ne 0 ]; then
-  eksctl create cluster --config-file ${SCRIPT_ROOT}/cluster/cluster.yaml
-else
+function fetchRepoUpdates() {
+  git branch --set-upstream-to=origin/master
+  git fetch
+  git rebase origin/master --autostash
+}
 
-  eksctl create iamserviceaccount --config-file ${SCRIPT_ROOT}/cluster/cluster.yaml --approve --override-existing-serviceaccounts
-  eksctl enable repo --config-file ${SCRIPT_ROOT}/cluster/cluster.yaml
-fi
+function installVerticalPodAutoscaler() {
+  set +e
+  kubectl get deployment -n kube-system vpa-updater >/dev/null 2>&1
+  retVal=$?
+  if [ $retVal -ne 0 ]; then
+    git clone --depth 1 git@github.com:kubernetes/autoscaler.git
+    cd autoscaler/vertical-pod-autoscaler/hack
+    ./vpa-up.sh
+    cd ../../../
+    rm -rf autoscaler
+  fi
+}
 
-set +e
-kubectl get deployment -n kube-system vpa-updater >/dev/null 2>&1
-retVal=$?
-if [ $retVal -ne 0 ]; then
-  git clone --depth 1 git@github.com:kubernetes/autoscaler.git
-  cd autoscaler/vertical-pod-autoscaler/hack
-  ./vpa-up.sh
-  cd ../../../
-  rm -rf autoscaler
-fi
+function createOrUpdateCluster() {
+
+  eksctl get cluster $(getClusterName) >/dev/null 2>&1
+  retVal=$?
+  set -e
+  if [ $retVal -ne 0 ]; then
+    eksctl create cluster --config-file ${SCRIPT_ROOT}/cluster/cluster.yaml
+  else
+
+    eksctl create iamserviceaccount --config-file ${SCRIPT_ROOT}/cluster/cluster.yaml --approve --override-existing-serviceaccounts
+    eksctl enable repo --config-file ${SCRIPT_ROOT}/cluster/cluster.yaml
+    eksctl enable profile --config-file ${SCRIPT_ROOT}/cluster/cluster.yaml
+  fi
+
+}
+
+createOrUpdateCluster
+
+fetchRepoUpdates
+
+installVerticalPodAutoscaler
 
 set -e
 helm upgrade -i helm-operator-secrets fluxcd/helm-operator --set helm.versions=v3 --namespace flux --set workers=40 --set allowNamespace=external-secrets
 helm upgrade -i helm-operator-ingress fluxcd/helm-operator --set helm.versions=v3 --namespace flux --set workers=40 --set allowNamespace=ingress-nginx
+kubectl apply -f ${SCRIPT_ROOT}/base/namespaces
+kubectl apply -f ${SCRIPT_ROOT}/base/releases/ingress-nginx
+kubectl apply -f ${SCRIPT_ROOT}/base/releases/external-secrets
 
-set +e
+
 waitForDeployment ingress-nginx external-ingress
-
 set -e
 kubectl rollout status deployment -n ingress-nginx external-ingress-nginx-ingress-controller
 
-set +e
-waitForDeployment external-secrets external-secrets
 
+waitForDeployment external-secrets external-secrets
 set -e
 kubectl rollout status deployment -n external-secrets external-secrets-kubernetes-external-secrets
 
-helm upgrade -i helm-operator-chartmuseum fluxcd/helm-operator --set helm.versions=v3 --namespace flux --set workers=40 --set allowNamespace=chartmuseum
 
-set +e
+helm upgrade -i helm-operator-chartmuseum fluxcd/helm-operator --set helm.versions=v3 --namespace flux --set workers=40 --set allowNamespace=chartmuseum
+kubectl apply -f ${SCRIPT_ROOT}/base/releases/chartmuseum
 waitForDeployment chartmuseum chartmuseum
 
 helm delete -n flux helm-operator-chartmuseum
 helm delete -n flux helm-operator-secrets
 helm delete -n flux helm-operator-ingress
 
+kubectl get hr -A
+
 kubectl get hr -A | grep -vi NAMESPACE | grep -E '(failed)' | awk '{printf "kubectl delete hr -n %s %s\n",$1,$2}' | xargs -I{} -t bash -c '{}'
 restartDeployment "flux" "helm-operator-private"
-
 kubectl get hr -A
--- hack/teardown.sh
+++ hack/teardown.sh
@@ -9,15 +9,14 @@ if [[ -z "${GITHUB_TOKEN}" ]]; then
   exit 1
 fi
 
-
-clusterName=$(yq .metadata.name - < cluster/cluster.yaml)
+clusterName=$(yq .metadata.name - <cluster/cluster.yaml)
 clusterName=$(echo "$clusterName" | sed -e 's/^"//' -e 's/"$//')
 
-eksctl get cluster "$clusterName"
+eksctl get cluster "$clusterName" >/dev/null 2>&1
 retVal=$?
 
 set -e
 
 if [ $retVal -eq 0 ]; then
-    eksctl delete cluster --name $clusterName
+  eksctl delete cluster --name "$clusterName"
 fi
